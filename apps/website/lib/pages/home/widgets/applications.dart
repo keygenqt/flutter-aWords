569544305856057c2a550a9bafc678332598e036
Vitaliy Zarubin <keygenqt@gmail.com>
2023-02-16 21:03:54 +0300

    Added help block

--- apps/website/lib/pages/home/widgets/applications.dart
+++ apps/website/lib/pages/home/widgets/applications.dart
@@ -1,9 +1,10 @@
+import 'dart:js' as js;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:website/theme/colors.dart';
 import 'package:website/theme/radius.dart';
 import 'package:website/widgets/containers/page_item.dart';
-import 'dart:js' as js;
 
 class HomeApplicationsWidget extends StatelessWidget {
   const HomeApplicationsWidget({super.key});
@@ -69,7 +70,7 @@ class HomeApplicationsWidget extends StatelessWidget {
                               color: Colors.white,
                               child: InkWell(
                                 borderRadius:
-                                MediaQuery.of(context).size.width < 400 ? AppRadius.extraLarge : AppRadius.small,
+                                    MediaQuery.of(context).size.width < 400 ? AppRadius.extraLarge : AppRadius.small,
                                 onTap: () {
                                   js.context.callMethod('open', ['https://www.android.com/']);
                                 },
--- apps/website/lib/pages/home/widgets/help.dart
+++ apps/website/lib/pages/home/widgets/help.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:lottie/lottie.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:website/theme/colors.dart';
+import 'package:website/theme/radius.dart';
 import 'package:website/widgets/containers/page_item.dart';
 
 class HomeHelpWidget extends StatelessWidget {
@@ -10,16 +14,48 @@ class HomeHelpWidget extends StatelessWidget {
     return Container(
       color: AppColors.backgroundLightSecondary,
       child: Padding(
-        padding: const EdgeInsets.only(top: 100, bottom: 100, left: 30, right: 30),
+        padding: const EdgeInsets.only(top: 80, bottom: 80, left: 30, right: 30),
         child: PageItemWidget(
-          child: SizedBox(
-            width: double.infinity,
-            child: Center(
-              child: Text(
-                'HomeHelpWidget',
-                style: Theme.of(context).textTheme.titleLarge,
+          child: Column(
+            children: [
+              Text(
+                AppLocalizations.of(context)!.home_help_title,
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(color: AppColors.primary),
               ),
-            ),
+              const SizedBox(height: 30),
+              Text(
+                AppLocalizations.of(context)!.home_help_text,
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+              ),
+              Lottie.asset(
+                'lottie/email.json',
+                width: 200,
+                fit: BoxFit.contain,
+              ),
+              Material(
+                color: AppColors.secondary,
+                borderRadius: AppRadius.small,
+                child: InkWell(
+                  borderRadius: AppRadius.small,
+                  onTap: () async {
+                    await launchUrl(Uri.parse('mailto:keygenqt@gmail.com'));
+                  },
+                  child: ClipRRect(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 14,
+                      ),
+                      child: Text(
+                        AppLocalizations.of(context)!.home_help_btn,
+                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
