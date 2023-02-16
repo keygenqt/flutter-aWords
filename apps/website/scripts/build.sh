36758bc77c6248b273b820b7a4b4606fc988c913
Vitaliy Zarubin <keygenqt@gmail.com>
2023-02-16 15:06:05 +0300

    Update build

--- apps/website/scripts/build.sh
+++ apps/website/scripts/build.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+cd ../
+
+## localization convert
+parse-hocon lib/l10n/app_en.conf > lib/l10n/app_en.arb
+parse-hocon lib/l10n/app_ru.conf > lib/l10n/app_ru.arb
+
 ## flutter
 flutter clean
 flutter pub get
 
-## build localization
-sh ./l10n.sh
+## gen localization run
+flutter gen-l10n
 
 ## build web
 flutter build web
--- apps/website/scripts/l10n.sh
+++ apps/website/scripts/l10n.sh
@@ -5,5 +5,8 @@ cd ../
 ## localization convert
 parse-hocon lib/l10n/app_en.conf > lib/l10n/app_en.arb
 parse-hocon lib/l10n/app_ru.conf > lib/l10n/app_ru.arb
-## run flutter
+
+## gen localization run
 flutter gen-l10n
+
+cd scripts || exit
