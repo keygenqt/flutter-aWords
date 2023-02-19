9002dbdfa08789bb136185c5eb6858d1f27f0794
Vitaliy Zarubin <keygenqt@gmail.com>
2023-02-19 12:50:04 +0300

    test add cookies

--- apps/server/lib/src/apps/api/routing/login.dart
+++ apps/server/lib/src/apps/api/routing/login.dart
@@ -1,20 +1,16 @@
 import 'dart:io';
 
-import 'package:mason_logger/mason_logger.dart';
 import 'package:server_awords/exports/apps/api/app.dart';
 import 'package:server_awords/exports/apps/api/validates.dart';
 import 'package:server_awords/exports/db/models.dart';
 import 'package:server_awords/exports/db/services.dart';
 import 'package:server_awords/exports/other/extensions.dart';
 import 'package:server_awords/src/base/app_di.dart';
-import 'package:uuid/uuid.dart';
 
 /// Route API REST for [Routes.login]
 class LoginRoute implements Route {
   @override
   String path = Routes.login.path;
-
-  Logger get _logger => getIt<Logger>();
 
   UsersService get _serviceUsers => getIt<UsersService>();
 
@@ -58,6 +54,11 @@ class LoginRoute implements Route {
               createAt: DateTime.now(),
             ),
           ]);
+
+          // test cookies
+          request.cookies.add(Cookie('session1', auth.first.token));
+          request.response.cookies.add(Cookie('session2', auth.first.token));
+
           // write data
           request.writeJson(auth.first);
         },
