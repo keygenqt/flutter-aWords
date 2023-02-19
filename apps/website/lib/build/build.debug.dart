4df8e001491f893702add24aedf8cbe52b0befc4
Vitaliy Zarubin <keygenqt@gmail.com>
2023-02-19 12:16:04 +0300

    Disable CORS, API localhost

--- apps/website/lib/build/build.debug.dart
+++ apps/website/lib/build/build.debug.dart
@@ -1,9 +1,12 @@
 import 'build.config.dart';
 
+/// Disable CORS
+/// Android Studio add arguments run web (Additional run args)
+/// --web-browser-flag "--disable-web-security" --web-port=3001
 class BuildDebugConfig implements BuildConfig {
   @override
   bool get isDebug => true;
 
   @override
-  String get baseUrl => 'https://awords-api.keygenqt.com/';
+  String get baseUrl => 'http://localhost:3000/';
 }
